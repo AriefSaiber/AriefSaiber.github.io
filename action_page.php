d24887acd0eba4d1608c18bde5c8ba64115a308c
ariefsaiber <ariefsaiber@gmail.com>
2021-04-20 00:38:44 +0800

    nais

---
+++ action_page.php
@@ -0,0 +1,11 @@
+<html><head><meta charset="utf-8">
+<title>Forms action page</title>
+<link rel="stylesheet" href="/w3css/4/w3.css">
+</head><body class="w3-container" data-new-gr-c-s-check-loaded="14.1006.0" data-gr-ext-installed="">
+<h1>Feedback was sent</h1>
+<h2>Thank you</h2><?php echo $_GET["Name"]?>
+<br>
+<?php echo $_GET["Email"]?>
+<br>
+
+</body></html>
